d453dc38c930fc376a48d571da34ca07143094b8
Aditya Kadam <139170382+Kadam-Aditya@users.noreply.github.com>
2024-09-15 10:43:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use("/api/generateImage/", generateImageRoute);
 app.use("/api/post/", posts);
